e3908ac750585c0766490ac5d382be70f4a164ce
Ryan <ryanmiller1517@gmail.com>
2024-02-18 10:25:55 -0500

    initial commit for baseclass lib

---
+++ baseclass/baseclasses.py
@@ -0,0 +1,189 @@
+from abc import ABC, abstractmethod
+import inspect
+from typing import Callable, List, Any
+
+from weakref import WeakSet
+from functools import wraps
+
+
+class TrackedInstances:
+
+    def __new__(cls, *args, **kwargs):
+        """
+        Track all instances of this class via weak references.
+
+        This set of instances does not include subclass instances,
+        but subclasses will inherit this ability.
+        """
+        instance = object.__new__(cls)
+        if 'instances' not in cls.__dict__.keys():
+            cls.instances = WeakSet()
+        cls.instances.add(instance)
+        return instance
+
+    @classmethod
+    def get_instances(cls, selector: Callable) -> set:
+        """
+        Search through all instances using the given selector, returning a list where selector is True.
+
+        :param selector: Callable
+            A callable (lambda or other function) that accepts one argument (an instance) and returns a bool.
+            Calls where selector == True will result in the corresponding instance being included in the
+            return.
+        """
+        if 'instances' not in cls.__dict__.keys():
+            raise AttributeError(f"{cls.__name__} class has no attribute 'instances'")
+        val = [v for v in cls.instances if selector(v)]
+        return set(val)
+
+    @classmethod
+    def inst_and_subcls_inst(cls, exclude=None) -> set:
+        """
+        Return a tuple of all instances of this class and its subclasses.
+
+        :param exceptions: List<class>
+            A list of subclasses (and subclasses thereof) to except from the instance query.
+        """
+        exclude = exclude or []
+        inst = list(cls.instances)
+        for subcls in [v for v in cls.__subclasses__() if v not in exclude]:
+            inst += subcls.inst_and_subcls_inst(exclude=exclude)
+        return set(inst)
+
+
+class CallPostInit(ABC):
+    
+    def __init_subclass__(cls, *primeargs, **primekwargs):
+        super().__init_subclass__(*primeargs, **primekwargs)
+        @wraps(cls.__init__)
+        def new_init(self, *args, init=cls.__init__, **kwargs):
+            init(self, *args, **kwargs)
+            if type(self) == cls:
+                self.__post_init__(*args, **kwargs)
+        cls.__init__ = new_init
+
+    @abstractmethod
+    def __post_init__(self, *args, **kwargs):
+        """
+        Called after all __init__ statements are complete.
+
+        *args, **kwargs are all arguments passed at instancing.
+        """
+
+
+class InstancingArgsTracker(CallPostInit):
+    """
+    Baseclass for tracking what arguments were used to instantiate a class
+    """
+    _metadata: dict  # contains the class instancing args
+
+    def __post_init__(self, *args, **kwargs):
+        """
+        Track all arguments used to instantiate this class.
+        """
+        # make all args a list (to use 'pop' function), and make return dict
+        args = list(args)
+        data = {}
+
+        # iterate through all bases defined in the class's inheritance chain
+        # order of iteration will be C->B->A for class D inheriting from C,
+        # class C inheriting from B, and so on...
+        for base in inspect.getmro(self.__class__):
+            # identify parameters from the base class __init__ function def
+            params = inspect.signature(base).parameters
+            params_list = list(params.values())
+
+            # build list of all positional arguments (where default value is empty)
+            arg = {
+                k:v for k, v in params.items()
+                if v.default is inspect.Parameter.empty and
+                v.kind not in [inspect.Parameter.VAR_POSITIONAL,
+                               inspect.Parameter.VAR_KEYWORD]
+            }
+            # build list of all kw args (where default value defined)
+            kw = {
+                k:v.default for k, v in params.items()
+                if v.default is not inspect.Parameter.empty and
+                v.kind not in [inspect.Parameter.VAR_POSITIONAL,
+                               inspect.Parameter.VAR_KEYWORD]
+            }
+
+            # insert default arguments into the return dict
+            data.update(**kw)
+
+            # for all positional arguments, assign passed values
+            for k, v in arg.items():
+                data[k] = args.pop(0) if len(args) > 0 else v.default
+
+            # if only positional arguments passed, override default kwarg
+            # value with positional argument
+            for i in range(len(args)):
+                idx = len(arg) + i  # parameter index to set value of
+                # it's possible we have more positional arguments than parameters
+                # when using positional args to pass forward to parent classes
+
+                # don't try assignment if you encounter a *args or **kwargs 
+                if params_list[idx].kind in [inspect.Parameter.VAR_POSITIONAL, 
+                                             inspect.Parameter.VAR_KEYWORD]:
+                    break
+                data[params_list[idx].name] = args.pop(0)
+
+            # if no additional arguments were passed to instantiate a parent
+            # class with **kwargs call, we are done
+            # (speeds execution by approximately 4x by eliminating depth of
+            #  iteration)
+            kinds = [x.kind for x in params_list]
+            if (inspect.Parameter.VAR_POSITIONAL not in kinds and
+                inspect.Parameter.VAR_KEYWORD not in kinds):
+                    break
+        
+        # update return dictionary with any kwargs dict passed to the instance
+        data.update(**kwargs)
+        
+        # clean up any 'cls', 'self', 'args', or 'kwargs' parameters, as these
+        # are of no use to us
+        for key in ['cls', 'self', 'args', 'kwargs']:
+            data.pop(key, None)
+        
+        # store the result to the instance _metadata variable
+        self._metadata = data
+
+    @property
+    def metadata(self):
+        """Return the instantiation variables for this class"""
+        return self._metadata
+
+    @classmethod
+    def get_mro_parameters(cls) -> List[inspect.Parameter]:
+        """
+        Return a list of parameters for the full MRO of the class.
+        """
+        rtn = []
+        for base in inspect.getmro(cls):
+            rtn += inspect.signature(base).parameters.values()
+        return {v for v in rtn if v.kind not in [inspect.Parameter.VAR_POSITIONAL, 
+                                                 inspect.Parameter.VAR_KEYWORD]}
+
+
+class EnumDict:
+    """
+    The EnumDict class allows us to access enum elements using dictionary
+    functions, which may be desireable in some cases.
+
+    EnumDicts are intended to be immutable in runtime, so new or existing
+    value assignments are not permitted.
+    """
+    @classmethod
+    def keys(cls) -> set:
+        return {k for k, v in cls.__dict__.items() if not any(
+            [k.startswith('__'), inspect.isfunction(v), inspect.ismethod(v)])}
+
+    @classmethod
+    def values(cls) -> set:
+        return {v for k, v in cls.__dict__.items() if not any(
+            [k.startswith('__'), inspect.isfunction(v), inspect.ismethod(v)])}
+
+    @classmethod
+    def __setattr__(cls, key: str, value: Any):
+        raise RuntimeError(f"EnumDict '{cls.__name__}'" +
+                           "cannot add or change values during runtime.")
---
+++ baseclass/tests/test_baseclasses.py
@@ -0,0 +1,240 @@
+# 12/9/2023 By MountainEclipse
+
+import unittest
+from inspect import Parameter
+from .. import baseclasses
+
+
+class TestTrackedInstances(unittest.TestCase):
+
+    class Parent(baseclasses.TrackedInstances):
+        pass
+
+    class Child(Parent):
+        pass
+
+    class Grandchild(Child):
+        pass
+
+    def test_new(self):
+        # when creating an instance of a class with tracked instances, 
+        # an 'instances' variable should be created as a WeakSet, and the
+        # new instance should be added.
+        p1 = self.Parent()
+        p2 = self.Parent()
+
+        self.assertTrue(hasattr(self.Parent, 'instances'))
+        self.assertEqual(set(self.Parent.instances), {p1, p2})
+
+        p3 = self.Parent()
+        self.assertEqual(set(self.Parent.instances), {p1, p2, p3})
+
+    def test_get_instances(self):
+        # when instances have been created, a specific instance can be retrieved
+        # using the get_instance function, which selects the instance based
+        # on a function that returns a bool (true for match, false otherwise)
+        self.assertRaises(AttributeError, self.Parent.get_instances, 
+                          selector=lambda x: x.value==10)
+        
+        p1 = self.Parent()
+        p1.value = 10
+
+        p2 = self.Parent()
+        p2.value = 13
+
+        # should return a set object
+        self.assertIsInstance(self.Parent.get_instances(lambda x: False), set)
+
+        # should return the p1 instance as a set
+        self.assertEqual(
+            self.Parent.get_instances(lambda x: x.value == p1.value),
+            {p1})
+        
+        # should return the p2 instance as a set
+        self.assertEqual(
+            self.Parent.get_instances(lambda x: x.value == p2.value),
+            {p2})
+
+    def test_inst_listing(self):
+        # all instances and subclass instances can be returned when needed.
+        # certain subclasses can be excluded, if an iterable is passed to
+        # the 'exclude' parameter
+        p1 = self.Parent()
+        p2 = self.Parent()
+        c1 = self.Child()
+        c2 = self.Child()
+        gc = self.Grandchild()
+
+        # should return all class and subclass instances
+        self.assertEqual(self.Parent.inst_and_subcls_inst(), {p1, p2, c1, c2, gc})
+
+        # should return all class and subclass instances
+        self.assertEqual(self.Child.inst_and_subcls_inst(), {c1, c2, gc})
+
+        # should return all class and subclass instances, except grandchildren
+        self.assertEqual(self.Parent.inst_and_subcls_inst(exclude=[self.Grandchild]), {p1, p2, c1, c2})
+
+
+class TestCallPostInit(unittest.TestCase):
+
+    class Superclass():
+        def __init__(self, value):
+            self.value = value
+
+        def __post_init__(self, value):
+            self.value = value + 1
+
+    class Subclass(Superclass, baseclasses.CallPostInit):
+        pass
+
+    def test_init_subclass(self):
+        # when subclassing this base class, the original init function will
+        # be overridden with a new one which appends a function call to
+        # a function __post_init__
+        
+        no_post = self.Superclass(10)
+
+        # no post-init function call should be made here
+        self.assertEqual(no_post.value, 10)
+
+        ya_post = self.Subclass(10)
+
+        # post-init function should be called, adding 1 to the passed value
+        self.assertEqual(ya_post.value, 11)
+
+
+class TestInstanceArgs(unittest.TestCase):
+
+    class Parent(baseclasses.InstancingArgsTracker):
+        def __init__(self, p1, p2, p3="vp3"):
+            pass
+    
+    class Child(Parent):
+        def __init__(self, c1, c2="vc2", c3="vc3", *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+    class Grandchild(Child):
+        def __init__(self, gc1, gc2="vgc2", *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_arg_assignment_parent(self) -> None:
+        test_args = {
+            "p1": "argp1",
+            "p2": "argp2",
+            "p3": "argp3"
+        }
+
+        # test assignment using only positional arguments
+        p = self.Parent(test_args['p1'], test_args['p2'], test_args['p3'])
+        self.assertEqual(set(p.metadata.keys()), {'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, test_args)
+
+        # test assignment using mix of positional and named arguments
+        p = self.Parent(test_args['p1'], p2=test_args['p2'], p3=test_args['p3'])
+        self.assertEqual(set(p.metadata.keys()), {'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, test_args)
+
+        # test assignment using kwargs dict passing
+        p = self.Parent(**test_args)
+        self.assertEqual(set(p.metadata.keys()), {'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, test_args)
+
+    def test_arg_assignment_child(self) -> None:
+        ta = {
+            "c1": "argc1",
+            "c2": "argc2",
+            "c3": "argc3",
+            "p1": "argp1",
+            "p2": "argp2",
+            "p3": "argp3"
+        }
+
+        # test assignment using only positional arguments
+        p = self.Child(*ta.values())
+        self.assertEqual(set(p.metadata.keys()), 
+                         {'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), 
+                         {'argc1', 'argc2', 'argc3', 'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, ta)
+
+        # test assignment using mix of positional and named arguments
+        p = self.Child(ta['c1'], ta['c2'], c3=ta['c3'], p1=ta['p1'], p2=ta['p2'], p3=ta['p3'])
+        self.assertEqual(set(p.metadata.keys()), {'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'argc1', 'argc2', 'argc3', 'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, ta)
+
+        # test assignment using kwargs dict passing
+        p = self.Child(**ta)
+        self.assertEqual(set(p.metadata.keys()), {'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'argc1', 'argc2', 'argc3', 'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, ta)
+
+    def test_arg_assignment_grandchild(self) -> None:
+        ta = {
+            "gc1": "arggc1",
+            "gc2": "arggc2",
+            "c1": "argc1",
+            "c2": "argc2",
+            "c3": "argc3",
+            "p1": "argp1",
+            "p2": "argp2",
+            "p3": "argp3"
+        }
+        
+        # test assignment using only positional arguments
+        p = self.Grandchild(*ta.values())
+        self.assertEqual(set(p.metadata.keys()), 
+                         {'gc1', 'gc2', 'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), 
+                         {'arggc1', 'arggc2', 'argc1', 'argc2', 'argc3', 'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, ta)
+
+        # test assignment using mix of positional and named arguments
+        p = self.Grandchild(ta['gc1'], ta['gc2'], ta['c1'], ta['c2'], c3=ta['c3'], p1=ta['p1'], p2=ta['p2'], p3=ta['p3'])
+        self.assertEqual(set(p.metadata.keys()), {'gc1', 'gc2', 'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'arggc1', 'arggc2', 'argc1', 'argc2', 'argc3', 'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, ta)
+
+        # test assignment using kwargs dict passing
+        p = self.Grandchild(**ta)
+        self.assertEqual(set(p.metadata.keys()), {'gc1', 'gc2', 'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+        self.assertEqual(set(p.metadata.values()), {'arggc1', 'arggc2', 'argc1', 'argc2', 'argc3', 'argp1', 'argp2', 'argp3'})
+        self.assertEqual(p.metadata, ta)
+
+    def test_MRO_params(self) -> None:
+        mro_params = self.Parent.get_mro_parameters()
+        self.assertEqual(len(mro_params), 3)
+        self.assertEqual({x.name for x in mro_params}, {'p1', 'p2', 'p3'})
+
+        mro_params = self.Grandchild.get_mro_parameters()
+        self.assertEqual(len(mro_params), 8)
+        self.assertEqual({x.name for x in mro_params}, 
+                         {'gc1', 'gc2', 'c1', 'c2', 'c3', 'p1', 'p2', 'p3'})
+
+
+class TestEnumDict(unittest.TestCase):
+
+    class ED(baseclasses.EnumDict):
+        KEY1 = "value1"
+        KEY2 = "value2"
+        KEY3 = "value3"
+
+    def test_access_funcs(self):
+        # ensure the 'keys' function returns all keys of the enum
+        self.assertEqual(self.ED.keys(), {"KEY1", "KEY2", "KEY3"})
+
+        # ensure the 'values' function returns all values of the enum
+        self.assertEqual(self.ED.values(), {"value1", "value3", "value2"})
+
+        # attribute setting not permitted for this
+        self.assertRaises(RuntimeError, self.ED.__setattr__, key="KEY4", value="value4")
+    
